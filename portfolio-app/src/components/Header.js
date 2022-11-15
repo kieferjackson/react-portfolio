3a407e77ca3335a957b8eb8ca8f93c01b419829a
kieferjackson <kieferleejackson@gmail.com>
2022-11-15 16:06:08 -0700

    Updated Header/Navbar

--- portfolio-app/src/components/Header.js
+++ portfolio-app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header>
-      <h1>Home</h1>
+      <h1>Kiefer Jackson</h1>
     </header>
   );
 }
--- portfolio-app/src/components/Navigation.js
+++ portfolio-app/src/components/Navigation.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 function Navigation() {
   return (
-    <div>
-      <h1>Nav Menu</h1>
-    </div>
+    <nav>
+      <ul>
+        <li>About Me</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
   );
 }
 
