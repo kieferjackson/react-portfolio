73a4ab3799270e38b7d819b28121306c3430e815
kieferjackson <kieferleejackson@gmail.com>
2022-11-18 11:51:31 -0700

    Styling changes + styling for header/footer components

--- portfolio-app/src/components/Footer.js
+++ portfolio-app/src/components/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+const footer_style = 
+{
+    padding: 30,
+    textAlign: 'center'
+}
+
 function Footer() {
   return (
-    <footer>
+    <footer style={footer_style}>
       <h1>This website was created by Kiefer Jackson using React</h1>
     </footer>
   );
--- portfolio-app/src/components/Header.js
+++ portfolio-app/src/components/Header.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Navigation from './Navigation';
 
+const styles =
+{
+    header: 
+    {
+        backgroundColor: '#2c2c2c1e',
+        padding: 20,
+        boxShadow: '0px 4px 8px 0px #85858567'
+    },
+    h1: { margin: 0 },
+}
+
 function Header({ page_view, setPage }) {
   return (
-    <header>
-      <h1>Kiefer Jackson</h1>
+    <header style={styles.header}>
+      <h1 style={styles.h1}>Kiefer Jackson</h1>
       <Navigation page_view={page_view} setPage={setPage} />
     </header>
   );
