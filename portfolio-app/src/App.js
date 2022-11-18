16191df2e61522bf907ce9d94a24c76beb722253
kieferjackson <kieferleejackson@gmail.com>
2022-11-18 11:33:50 -0700

    Implemented style sheet and updated components

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -2,17 +2,21 @@ import React, { useState } from 'react';
 import Header from './components/Header';
 import Page from './components/Page';
 import Footer from './components/Footer';
+// Importing style
+import './index.css';
 
 function App() {
-  const [page_view, setPage] = useState('About Me');
+	const [page_view, setPage] = useState('About Me');
 
-  return (
-    <div>
-      <Header page_view={page_view} setPage={setPage} />
-      <Page page_view={page_view} setPage={setPage} />
-      <Footer />
-    </div>
-  );
+	return (
+		<div>
+			<Header page_view={page_view} setPage={setPage} />
+			<main>
+				<Page page_view={page_view} setPage={setPage} />
+			</main>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- portfolio-app/src/components/Page.js
+++ portfolio-app/src/components/Page.js
@@ -10,29 +10,34 @@ function select_page(page)
             const HOST_URL = 'tiiny.host';
 
         return (
-            <p>
-                I currently live in Phoenix, Arizona, where I was born and raised. I studied chemistry at 
-                Arizona State University and received my BA for General Chemistry there in May 2022. 
-                <br /><br />
-                My interests include: graphic design (typography, illustration), technology, videogames, and
-                cooking.
-                <br /><br />
-                I began programming in the summer of 2021, when I started the development of a chemistry
-                program to aid in copolymer synthesis for my research mentor, written in C++. Later, I 
-                transitioned the C++ program to a web application written in JavaScript, which is currently unfinished.
-                <br /><br />
-                Since then, I designed and deployed the company website for Advocate Hospice and Palliative Care, 
-                which I hosted using <a href={`https://${HOST_URL}/`} title={`https://${HOST_URL}/`}>{HOST_URL}</a>.
-                In addition, I worked on Crypto-Info, where I was the project coordinator and responsible for 
-                fetching API data. As well, I created the backend component of Club Pet using Sequelize (among other 
-                Node.js technologies) in addition to large parts of its frontend code using Handlebars.
-            </p>
+            <section key='about_me_section'>
+                <h2>About Me</h2>
+                <p>
+                    I currently live in Phoenix, Arizona, where I was born and raised. I studied chemistry at 
+                    Arizona State University and received my BA for General Chemistry there in May 2022. 
+                    <br /><br />
+                    My interests include: graphic design (typography, illustration), technology, videogames, and
+                    cooking.
+                    <br /><br />
+                    I began programming in the summer of 2021, when I started the development of a chemistry
+                    program to aid in copolymer synthesis for my research mentor, written in C++. Later, I 
+                    transitioned the C++ program to a web application written in JavaScript, which is currently unfinished.
+                    <br /><br />
+                    Since then, I designed and deployed the company website for Advocate Hospice and Palliative Care, 
+                    which I hosted using <a href={`https://${HOST_URL}/`} title={`https://${HOST_URL}/`}>{HOST_URL}</a>.
+                    In addition, I worked on Crypto-Info, where I was the project coordinator and responsible for 
+                    fetching API data. As well, I created the backend component of Club Pet using Sequelize (among other 
+                    Node.js technologies) in addition to large parts of its frontend code using Handlebars.
+                </p>
+            </section>
         );
         case 'Portfolio':
             return (
-                <div className='projects_container'>
-                    {projects.map((project) => <Project key={project.title} {...project} />)}
-                </div>
+                <section key='portfolio_section'>
+                    <div className='projects_container'>
+                        {projects.map((project) => <Project key={project.title} {...project} />)}
+                    </div>
+                </section>
             );
         case 'Contact':
             const GITHUB_USERNAME = 'kieferjackson';
@@ -40,7 +45,7 @@ function select_page(page)
             const EMAIL_ADDRESS = 'kieferleejackson@gmail.com';
 
             return (
-                <section>
+                <section key='contact_section'>
                     <h3>Contact</h3>
                     <div className='contact_info'>
                         GitHub: <a href={`https://github.com/${GITHUB_USERNAME}/`}>{GITHUB_USERNAME}</a>
