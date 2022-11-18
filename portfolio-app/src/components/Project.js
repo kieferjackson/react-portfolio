602be9bd2631a1c07b15de1ea75e03b24de9cec5
kieferjackson <kieferleejackson@gmail.com>
2022-11-18 01:16:58 -0700

    Images now render for projects

--- portfolio-app/src/components/Project.js
+++ portfolio-app/src/components/Project.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Project({ title, languages_used, website_link, alt_tag }) {
+function Project({ title, languages_used, website_link, alt_tag, img }) {
     return (
         <div className='project_card' key={title}>
             <h3>{title}</h3>
@@ -8,7 +8,7 @@ function Project({ title, languages_used, website_link, alt_tag }) {
                 {languages_used.map((language, index) => <li key={index}>{language}</li>)}
             </ul>
             <a href={website_link} title={website_link}>
-                <img alt={alt_tag}></img>
+                <img src={img} alt={alt_tag}></img>
             </a>
         </div>
     );
--- portfolio-app/src/utils/projects.js
+++ portfolio-app/src/utils/projects.js
@@ -1,12 +1,19 @@
+import placeholder from './assets/placeholder.png';
+// Importing local image data
+import copolycalc from './assets/copoly-calc-preview.PNG';
+import ahpc from './assets/ahpc_logo.png';
+import cryptoinfo from './assets/crypto-info-preview.png';
+import clubpet from './assets/club-pet-preview.png';
 
 class Project 
 {
-    constructor(title, languages_used, website_link, alt_tag)
+    constructor(title, languages_used, website_link, alt_tag, img = placeholder)
     {
         this.title = title;
         this.languages_used = languages_used;
         this.website_link = website_link;
         this.alt_tag = alt_tag;
+        this.img = img;
     }
 }
 
@@ -16,6 +23,7 @@ class Project
  * @property {array} languages_used - The programming languages/technologies used in a project
  * @property {string} website_link - URL for the deployed project
  * @property {string} alt_tag - Alt property for a project's given image
+ * @property {image} img - Given image to represent a project
  */
 const projects = 
 [
@@ -27,7 +35,8 @@ const projects =
         "JavaScript"
     ],
     "https://kieferjackson.github.io/copoly-calc/",
-    "Copoly-Calc splash page including instructions for minimum user input"),
+    "Copoly-Calc splash page including instructions for minimum user input",
+    copolycalc),
     // Advocate Hospice Website
     new Project("Advocate Hospice Website", 
     [
@@ -36,7 +45,8 @@ const projects =
         "Bootstrap"
     ],
     "https://www.advocatehpc.com/",
-    "Advocate Hospice and Palliative Care logo"),
+    "Advocate Hospice and Palliative Care logo",
+    ahpc),
     // Crypto-Info
     new Project("Crypto-Info", 
     [
@@ -46,7 +56,8 @@ const projects =
         "JavaScript"
     ],
     "https://kieferjackson.github.io/crypto-info/",
-    "Logo for Crypto-Info website"),
+    "Logo for Crypto-Info website",
+    cryptoinfo),
     // Club Pet
     new Project("Club Pet", 
     [
@@ -58,7 +69,8 @@ const projects =
         "Sequelize"
     ],
     "https://club-pet.herokuapp.com/",
-    "Showing two pets of a user: King (male panda bear) and Cosmos (female dog)")
+    "Showing two pets of a user: King (male panda bear) and Cosmos (female dog)",
+    clubpet)
 ];
 
 export { projects };
