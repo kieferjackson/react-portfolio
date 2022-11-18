b182df7b01fbb78743c20528f668ae682bdf048c
kieferjackson <kieferleejackson@gmail.com>
2022-11-18 00:41:23 -0700

    Moved JSX generation back to components

--- portfolio-app/src/components/Page.js
+++ portfolio-app/src/components/Page.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Project from './Project';
-import { about_me, projects, contact } from '../utils';
+import { projects } from '../utils';
 
 function select_page(page) 
 {
     switch(page)
     {
         case 'About Me':
-            return about_me();
+            const HOST_URL = 'tiiny.host';
+
+        return (
+            <p>
+                I currently live in Phoenix, Arizona, where I was born and raised. I studied chemistry at 
+                Arizona State University and received my BA for General Chemistry there in May 2022. 
+                <br /><br />
+                My interests include: graphic design (typography, illustration), technology, videogames, and
+                cooking.
+                <br /><br />
+                I began programming in the summer of 2021, when I started the development of a chemistry
+                program to aid in copolymer synthesis for my research mentor, written in C++. Later, I 
+                transitioned the C++ program to a web application written in JavaScript, which is currently unfinished.
+                <br /><br />
+                Since then, I designed and deployed the company website for Advocate Hospice and Palliative Care, 
+                which I hosted using <a href={`https://${HOST_URL}/`} title={`https://${HOST_URL}/`}>{HOST_URL}</a>.
+                In addition, I worked on Crypto-Info, where I was the project coordinator and responsible for 
+                fetching API data. As well, I created the backend component of Club Pet using Sequelize (among other 
+                Node.js technologies) in addition to large parts of its frontend code using Handlebars.
+            </p>
+        );
         case 'Portfolio':
             return (
                 <div className='projects_container'>
@@ -15,7 +35,24 @@ function select_page(page)
                 </div>
             );
         case 'Contact':
-            return contact();
+            const GITHUB_USERNAME = 'kieferjackson';
+            const PHONE_NUMBER = '(602)-620-6428';
+            const EMAIL_ADDRESS = 'kieferleejackson@gmail.com';
+
+            return (
+                <section>
+                    <h3>Contact</h3>
+                    <div className='contact_info'>
+                        GitHub: <a href={`https://github.com/${GITHUB_USERNAME}/`}>{GITHUB_USERNAME}</a>
+                    </div>
+                    <div className='contact_info'>
+                        Phone: {PHONE_NUMBER}
+                    </div>
+                    <div className='contact_info'>
+                        Email: <a href={`mailto:${EMAIL_ADDRESS}`}>{EMAIL_ADDRESS}</a>
+                    </div>
+                </section>
+            );
         default:
             return <div>Nothing here...</div>
     }
--- portfolio-app/src/utils/index.js
+++ portfolio-app/src/utils/index.js
@@ -1,5 +1,3 @@
-import { about_me } from './about_me';
 import { projects } from './projects';
-import { contact } from './contact_info';
 
-export { about_me, projects, contact };
+export { projects };
